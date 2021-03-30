bd802f32dfb43a523f09a1f7b99f3f055e5b76c6
Crazylov3 <73377942+Crazylov3@users.noreply.github.com>
2021-03-30 13:03:50 +0700

    Add files via upload
    reup lab01,lab02. Correcting structure of branch

---
+++ Lab01/Ex6_5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Ex6_5 {
+    String name = "haha";
+    public static void main(String[]args){
+        double[] arr= { 1,3,4,5,2,8,7,6 };
+        for (int i= 0 ; i < arr.length ; i++){
+            
+            System.out.println(arr[i]);
+        }
+        
+    
+        
+    }
+}
