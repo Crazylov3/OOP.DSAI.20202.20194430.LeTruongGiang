27a30c4d8fc5644e2f828d6f5873b336a56d7e30
Crazylov3 <giang.lt194430@sis.hust.edu.vn>
2021-04-30 22:40:20 +0700

    Add lab04

--- AimsProject/src/hust/soict/dsai/test/cart/CartTest.java
+++ AimsProject/src/hust/soict/dsai/test/cart/CartTest.java
@@ -1,7 +1,10 @@
 package hust.soict.dsai.test.cart;
 
-import hust.soict.dsai.aims.disc.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
+import hust.soict.dsai.aims.media.Media;
 import hust.soict.dsai.aims.cart.Cart;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -12,16 +15,15 @@ public class CartTest {
         Cart cart = new Cart();
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction","George Lucas",87,24.85f);
-        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aldain","Animation",18.99f);
-        cart.addDVDs(dvd1);
-        cart.addDVDs(dvd2);
-        cart.addDVDs(dvd3);
-        cart.print();
-        cart.totalCost();
-        System.out.print("Enter the title: ");
-        DigitalVideoDisc.getDetail(sc.nextLine());
-        System.out.print("Enter the ID: ");
-        DigitalVideoDisc.getDetail(sc.nextInt());
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Star Wars", "Science Fiction","George Lucas",87,24.85f);
+
+        ArrayList<Media> list = new ArrayList<Media>();
+        list.add(dvd1);
+        list.add(dvd2);
+        list.add(dvd3);
+
+
+
     }
     
 }
